bf5d4a82f53910d072f074a88af24929f2dafff6
bernard-ng <ngandubernard@gmail.com>
2019-01-01 19:08:47 +0200

    [Fix] fix up the fetching bug

--- src/views/Podcasts/Show.vue
+++ src/views/Podcasts/Show.vue
@@ -1,30 +1,55 @@
 <template>
-  <div class="posdcasts-index">
-     <kbd>{{ podcast }}</kbd>
+  <div class="posdcasts-show">
+
+    <PodcastHero :podcast="podcast"></PodcastHero>
+
+    <main class="main" id="main">
+      <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi dolore ipsa consectetur tenetur fuga. Beatae accusantium laboriosam inventore mollitia quis fuga sapiente quia, perferendis corporis exercitationem, natus minima itaque ea?</div>
+      <div>Voluptate, harum quod consequatur vitae iste et assumenda asperiores inventore recusandae ad laboriosam sint cumque temporibus ducimus quas labore similique nemo. Culpa nobis, temporibus minima aperiam eos tempora obcaecati. Culpa.</div>
+      <div>Molestiae ad eum nemo enim cum eos aut fugit eveniet laborum ullam? Adipisci, veritatis modi maxime harum iure quisquam quas illum porro necessitatibus excepturi sunt consequatur eum fugit aliquam. Id.</div>
+      <div>Id ipsa quidem numquam delectus illum iure aut rem repellat dolorem quod, consequatur unde suscipit nesciunt, eius doloremque nihil voluptatem? Aperiam aut molestiae commodi, qui optio quae quas nemo fugiat.</div>
+      <div>Aliquam aperiam iusto reiciendis, autem perspiciatis ab nam id similique fugiat beatae nihil hic natus suscipit? Neque consequatur, veniam facilis, commodi autem sed illo officia eaque corrupti porro, expedita et!</div>
+      <div>Perspiciatis eius reiciendis, itaque laboriosam facilis voluptatem, at magnam accusamus unde facere beatae inventore nostrum minima, ducimus alias veniam quidem? A numquam, aliquam beatae officiis obcaecati quo reiciendis dolores quisquam.</div>
+      <div>Pariatur culpa impedit asperiores odio voluptatem repellendus, distinctio debitis non veritatis enim voluptas eveniet possimus, labore modi tempore assumenda, deserunt at cumque aliquam quisquam minus quod! Tenetur reiciendis deserunt corrupti?</div>
+      <div>Rerum et est pariatur temporibus perferendis aperiam, eveniet quos soluta! Velit ex error molestias, amet voluptatibus aspernatur autem incidunt provident, commodi aliquam minima nam tempore ipsa at hic maxime quaerat!</div>
+    </main>
   </div>
 </template>
 
 <script>
+import PodcastHero from '@/components/Podcasts/PodcastHero.vue'
+
 export default {
   name: 'podcast-show',
   data () {
     return  {
       action: '',
-      loading: false,
+      podcast: null,
       error: null,
-      podcast: null
     }
   },
-  created () {
-    let id = this.$route.params.id
-    let slug = this.$route.params.slug
-    this.loading = true
-
-    this.$http.get(`podcasts/${slug}-${id}`).then(r => {
-      this.podcast = r.data.podcast
-      this.action = r.data['api.action']
-      this.loading = false
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      vm.$http.get(`podcasts/${to.params.slug}-${to.params.id}`).then(r => {
+        vm.setData(r.data)
+      })
     })
-  }
+  },
+  beforeRouteUpdate (to, from, next) {
+    this.podcast = null
+    this.$http.get(`podcasts/${to.params.slug}-${to.params.id}`).then(r => {
+      this.action = r.data.action
+      this.podcast = r.data.podcast
+    })
+  },
+  methods: {
+    setData (data) {
+      this.action = data.action
+      this.podcast = data.podcast
+    }
+  },
+  components: {
+    PodcastHero
+  },
 }
 </script>
