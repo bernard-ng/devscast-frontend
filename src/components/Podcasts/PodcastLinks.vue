8fe99dbf1a0edbf5c0d20df9c72824d0bb6b9778
bernard-ng <ngandubernard@gmail.com>
2019-01-02 08:52:43 +0200

    [New] create PodcastLinks components for jumping into a specific part of a podcast

---
+++ src/components/Podcasts/PodcastLinks.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="podcast-links">
+    <div class="container">
+      <ul class="podcast-links-list">
+        <li><a href="#" class="btn btn-success btn-sm">00:51</a> <span class="podcast-link-cite">Jump intro</span></li>
+        <li><a href="#" class="btn btn-success btn-sm">02:10</a> <a href="#" target="_blank" class="podcast-link-cite">The Envato Market</a></li>
+        <li><a href="#" class="btn btn-success btn-sm">05:25</a> <a href="#" target="_blank" class="podcast-link-cite">How to upload a Theme</a></li>
+        <li><a href="#" class="btn btn-success btn-sm">07:32</a> <span class="podcast-link-cite">A basic comment without link</span></li>
+        <li><a href="#" class="btn btn-success btn-sm">09:00</a> <a href="#" target="_blank" class="podcast-link-cite">Learning from the basics</a></li>
+        <li><a href="#" class="btn btn-success btn-sm">12:15</a> <a href="#" target="_blank" class="podcast-link-cite">http://themeforest.net/</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PodcastLinks'
+}
+</script>
